b773cdd826a37dead78b6a3db7ec16e37269fb06
William Chen <44458534+funwilliam@users.noreply.github.com>
2023-11-30 10:00:57 +0800

    微調

--- app-sendMail/main.go
+++ app-sendMail/main.go
@@ -6,20 +6,31 @@ import (
 	"net/http"
 )
 
-// 创建一个结构体来匹配前端发送的JSON数据
 type UploadData struct {
 	Text  string `json:"text"`
 	Image string `json:"image"`
 }
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*") // 允许任何源
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	// 预检请求的处理
+	if r.Method == "OPTIONS" {
+		return
+	}
+
 	if r.Method != "POST" {
 		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	var data UploadData
-	// 解析JSON数据
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -27,11 +38,9 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	// 处理数据（例如打印到控制台）
 	fmt.Printf("Received text: %s\n", data.Text)
-	fmt.Printf("Received image: %s\n", data.Image)
+	fmt.Printf("Received image: v")
 
-	// 发送响应回前端
 	fmt.Fprintf(w, "Received text and image")
 }
 
