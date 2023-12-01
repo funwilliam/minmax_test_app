729f88597fe691774167fcc3a0ea2d8f73b8b912
William Chen <44458534+funwilliam@users.noreply.github.com>
2023-12-01 12:01:27 +0800

    調整

--- app-sendMail/main.go
+++ app-sendMail/main.go
@@ -95,6 +95,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to send email", http.StatusInternalServerError)
 		return
 	}
+	log.Printf(data.Text)
 	log.Printf("Email sent successfully")
 	fmt.Fprintf(w, "Email sent successfully")
 }
