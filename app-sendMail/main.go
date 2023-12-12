1e4976cf185cf02dd7cf8b850608eed6015b15b9
William Chen <44458534+funwilliam@users.noreply.github.com>
2023-12-12 14:46:02 +0800

    修改後端

--- app-sendMail/main.go
+++ app-sendMail/main.go
@@ -10,10 +10,10 @@ import (
 	"strings"
 )
 
-// SMTP 服务器设置
 const (
 	smtpHost = "smtp-pulse.com"
 	smtpPort = "2525"
+	sender   = "William@minmax.com.tw"
 )
 
 type UploadData struct {
@@ -27,14 +27,18 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
-func sendMail(login, password, to, subject, body, base64Image string) error {
-	auth := smtp.PlainAuth("", login, password, smtpHost)
+func sendMail(id, password, to, subject, body, base64File, encodeType, mediaType string) error {
+	// smtp伺服器身分驗證
+	auth := smtp.PlainAuth("", id, password, smtpHost)
 
-	// 使用更独特的边界标识符
+	// 邊界標示符
 	boundary := "my_unique_boundary"
 
-	// 构建邮件内容
-	from := "William@minmax.com.tw"
+	// 根據 MIME 類型確定檔案副檔名
+	fileExtension := strings.SplitN(mediaType, "/", 2)[1]
+
+	// 構建郵件內容
+	from := sender
 	msg := "From: " + from + "\n" +
 		"To: " + to + "\n" +
 		"Subject: " + subject + "\n" +
@@ -44,24 +48,30 @@ func sendMail(login, password, to, subject, body, base64Image string) error {
 		"Content-Type: text/plain; charset=utf-8\n\n" +
 		body + "\n\n"
 
-	// 添加图片附件
-	if base64Image != "" {
+	// 圖片附檔
+	if base64File != "" && encodeType != "" && mediaType != "" {
 		msg += "--" + boundary + "\n" +
-			"Content-Type: image/png\n" +
-			"Content-Transfer-Encoding: base64\n" +
-			"Content-Disposition: attachment; filename=\"image.png\"\n\n" +
-			base64Image + "\n\n"
+			"Content-Type: " + mediaType + "\n" +
+			"Content-Transfer-Encoding: " + encodeType + "\n" +
+			"Content-Disposition: attachment; filename=\"attachment." + fileExtension + "\"\n\n" +
+			base64File + "\n\n"
 	}
 
-	// 邮件结束标识
+	// 郵件結束標示
 	msg += "--" + boundary + "--"
 
-	// 发送邮件
+	// 發送
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, []byte(msg))
 	return err
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	var data UploadData
+	var stmt string
+	var mediaType string
+	var encodeType string
+	var base64File string
+
 	enableCors(&w)
 
 	if r.Method == "OPTIONS" {
@@ -73,7 +83,6 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var data UploadData
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -81,9 +90,31 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	// 处理 Base64 图像数据
-	base64Image := strings.TrimPrefix(data.Image, "data:image/png;base64,")
-	note := data.Text
+	// 檢查是否同時沒有文字和圖片
+	if data.Text == "" && data.Image == "" {
+		http.Error(w, "Text and image are both empty", http.StatusBadRequest)
+		return
+	}
+
+	// 如果有文字
+	if data.Text != "" {
+		stmt = data.Text
+	}
+
+	// 如果有圖片，解碼圖片數據(base64編碼 -> 二進制數據) + 取得圖片格式
+	if data.Image != "" {
+		dataParts := strings.FieldsFunc(data.Image, func(r rune) bool {
+			return r == ';' || r == ','
+		})
+
+		if len(dataParts) != 3 {
+			http.Error(w, "Invalid image data", http.StatusBadRequest)
+			return
+		}
+		mediaType = dataParts[0]
+		encodeType = dataParts[1]
+		base64File = dataParts[2]
+	}
 
 	// SMTP 配置
 	login := "minmax.ed.notification.sys@gmail.com" // 更改为您的 Gmail 地址
@@ -91,12 +122,12 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	to := "minmax.ed.notification.sys@gmail.com"    // 更改为收件人地址
 	subject := "測試"                                 // 邮件主题
 
-	if err := sendMail(login, password, to, subject, note, base64Image); err != nil {
+	if err := sendMail(login, password, to, subject, stmt, base64File, encodeType, mediaType); err != nil {
 		log.Printf("Failed to send email: %v\n", err)
 		http.Error(w, "Failed to send email", http.StatusInternalServerError)
 		return
 	}
-	log.Print(note)
+	log.Print("receive event: " + stmt)
 	log.Printf("Email sent successfully")
 	fmt.Fprintf(w, "Email sent successfully")
 }
